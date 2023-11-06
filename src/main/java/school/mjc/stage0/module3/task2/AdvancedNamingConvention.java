d33476a248c93ce10b2ffdb3e4e08cddcd40e0a4
Nicholas <urkona@list.ru>
2023-11-06 11:29:38 +0300

    init: init task

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,16 +1,14 @@
 package school.mjc.stage0.module3.task2;
 
-public class advancedNamingConvention {
-    private static final int adult_age = 18;
-
-    private int AGE;
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
-        //some code
+    void callToFriend() {
     }
 
-    void callByNumber(int Number) {
-        //some code
+    void callByNumber(int number) {
+
     }
 }
